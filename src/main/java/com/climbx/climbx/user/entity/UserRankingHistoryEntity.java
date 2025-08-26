22328f3c6372a8c07e489f860204fa62ae0d0d65
CHAEWAN KIM <66085474+amsminn@users.noreply.github.com>
2025-08-26 21:18:17 +0900

    [SWM-283] fix: 히스토리 엔티티에 유니크 제약 조건 추가 및 데이터 초기화 SQL에서 불필요한 레코드 삭제

--- src/main/java/com/climbx/climbx/user/entity/UserRankingHistoryEntity.java
+++ src/main/java/com/climbx/climbx/user/entity/UserRankingHistoryEntity.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -23,7 +24,10 @@ import lombok.experimental.Accessors;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
-@Table(name = "user_ranking_histories")
+@Table(name = "user_ranking_histories", uniqueConstraints = {
+    @UniqueConstraint(name = "uk_user_ranking_histories_user_date_criteria", 
+                     columnNames = {"user_id", "created_at", "criteria"})
+})
 @SQLRestriction("deleted_at IS NULL")
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @AllArgsConstructor(access = lombok.AccessLevel.PRIVATE)
