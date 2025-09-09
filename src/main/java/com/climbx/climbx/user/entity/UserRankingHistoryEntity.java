f74db6e9de862bec13c2c5f52a10dfed2270c120
CHAEWAN KIM <66085474+amsminn@users.noreply.github.com>
2025-09-09 20:33:37 +0900

    [SWM-296] refact: UserRankingHistoryEntity에서 불필요한 import 제거

--- src/main/java/com/climbx/climbx/user/entity/UserRankingHistoryEntity.java
+++ src/main/java/com/climbx/climbx/user/entity/UserRankingHistoryEntity.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
